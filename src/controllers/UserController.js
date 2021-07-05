efe90e4f13ca512a3b6d83a6706d6a2b04523bb5
gabriel scalese <cc19171@g.unicamp.br>
2021-07-05 17:00:22 -0300

    Updated User Controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -39,8 +39,8 @@ module.exports = {
     const transport = nodemailer.createTransport({
       service: "Gmail",
       auth: {
-        user: process.env.USER,
-        pass: process.env.PASS,
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
